36385f43880532a6a40bf89fddaa7774acfe1439
frankgabrielle1107 <daoanagabrielle@gmail.com>
2026-05-08 14:21:26 +0800

    Added validation

--- script.js
+++ script.js
@@ -4,16 +4,26 @@ function login(){
     let pass = document.getElementById("password").value;
     let message = document.getElementById("message");
 
+    // Empty validation
     if(user == "" || pass == ""){
 
-        message.innerHTML = "Please fill all fields";
+        message.innerHTML = "Please fill in all fields!";
         message.style.color = "red";
 
     }
 
+    // Username minimum validation
+    else if(user.length < 3){
+
+        message.innerHTML = "Username must be at least 3 characters!";
+        message.style.color = "orange";
+
+    }
+
+    // Correct login
     else if(user == "admin" && pass == "1234"){
 
-        message.innerHTML = "Login Successful";
+        message.innerHTML = "Login Successful!";
         message.style.color = "green";
 
         setTimeout(function(){
@@ -24,9 +34,10 @@ function login(){
 
     }
 
+    // Wrong credentials
     else{
 
-        message.innerHTML = "Wrong username or password";
+        message.innerHTML = "Invalid username or password!";
         message.style.color = "red";
 
     }
